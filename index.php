8c759539617ba3fd96b269320c3294fb6c2cfa52
yogiofficemail <yogiofficemail@gmail.com>
2024-02-02 12:34:58 +0530

    select with search

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Select 2 </title>
+    <link rel="stylesheet" href="select2.css">
+</head>
+
+<body>
+    <select name="" id="" class="select_with_search">
+        <option value="">hai</option>
+        <option value="">hai2</option>
+    </select>
+</body>
+<script src="jquery.js"></script>
+<script src="select2.js"></script>
+<script>
+$(document).ready(function() {
+    $('.select_with_search').select2();
+});
+</script>
+
+</html>
